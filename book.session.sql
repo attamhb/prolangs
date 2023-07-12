3099592d4dd21b87cc9e65f4cf341da0c2ddd2bb
theodcod <attahassani@hotmail.com>
2023-07-11 23:44:21 -0700

    updated

---
+++ book.session.sql
@@ -0,0 +1,11 @@
+SELECT geo_name,
+--state_us_abbreviation AS "st",
+--p0010001 AS "Total Population",
+--p0010003 AS "White Alone",
+--p0010004 AS "Black or African American Alone",
+--p0010005 AS "Am Indian/Alaska Native Alone",
+--p0010006 AS "Asian Alone",
+--p0010007 AS "Native Hawaiian and Other Pacific Islander Alone",
+--p0010008 AS "Some Other Race Alone",
+--p0010009 AS "Two or More Races"
+FROM us_counties_2019;
